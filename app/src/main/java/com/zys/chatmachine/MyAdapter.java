7ac318b95e25bda5b6806343fa69a0a18b6a98fa
Jayson-Zhou <jayson-zys@foxmail.com>
2018-09-21 14:03:29 +0800

    基本完成聊天布局

---
+++ app/src/main/java/com/zys/chatmachine/MyAdapter.java
@@ -0,0 +1,10 @@
+package com.zys.chatmachine;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by zm678 on 2018/9/21.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/zys/chatmachine/ResponceResult.java
@@ -0,0 +1,8 @@
+package com.zys.chatmachine;
+
+/**
+ * Created by zm678 on 2018/9/21.
+ */
+
+public class ResponceResult {
+}
