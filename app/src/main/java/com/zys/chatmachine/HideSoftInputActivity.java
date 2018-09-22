64cda852f6d0104718afe67e2b0dab7e50826136
Jayson-Zhou <jayson-zys@foxmail.com>
2018-09-22 23:07:41 +0800

    最终版，对界面进行了优化

---
+++ app/src/main/java/com/zys/chatmachine/HideSoftInputActivity.java
@@ -0,0 +1,12 @@
+package com.zys.chatmachine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HideSoftInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
